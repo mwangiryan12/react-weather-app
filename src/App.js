585531c53436c69773a1d860704f5427b5982d80
mwangiryan12 <162281245+mwangiryan12@users.noreply.github.com>
2024-05-08 23:39:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {WEATHER_API_URL, WEATHER_API_KEY} from ""
+import { useState } from 'react';
+
+import Search from '.compnonents/search/search';
+//import {'api_link'} from '..api';
+import currentWeather from './components/current-weather/current-weather';
+import 
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        {/* Define route for Detailedforecast.js */}
-        <Route path="/detailed-forecast/:weatherId" component={Detailedforecast} />
-        {/* Other routes */}
-      </Switch>
-    </Router>
-  );
+  
+    const[];
+    const[];
+  
+    const handleOnSearch = (searchData) => {
+      //console.log(searchData);
+      const [ lat,lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch =  fetch(`{}  )
+    const forecastFetch = fetch()
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async(repsonse)) =>{
+          const weatherResponse = await response[0] .json();
+          const foreacastResponse = await response[1] .json();
+
+        }
+          setCurrentWeather({city: searchData.label, ...weatherResponse);
+          setForecast({city: searchData.label, ...forecastResponse});
+    //api
+   }
+    return (
+      <div className = "container">
+        <Search onSearchChange ={handleOnSearch} />
+        {currentWeather && <currentWeather data={currentWeather}/>}
+        {forecast &&<Forecast data={}/>}
+        
+      </div>
+  
+        
+        
+      
+  
+    )
 
   
 }
