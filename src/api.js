441d19d559581b6e6096255ae6b77bbb5ebb9a1b
mwangiryan12 <mwangiryan12@gmail.com>
2024-05-09 22:04:02 +0300

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "92d20a01d9mshde02ff138aebcc8p19129ajsn33e8f3ca4dc9",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "3244003e0169aab7a2182966fb865021"; // enter your key from openweather API
