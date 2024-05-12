ee1536960e216f661d2fd2f448e4438d687719f8
mwangiryan12 <162281245+mwangiryan12@users.noreply.github.com>
2024-05-12 22:01:58 +0300

    Update Search.js

--- src/components/city-search/Search.js
+++ src/components/city-search/Search.js
@@ -1,43 +1,43 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_API_URL, geoApiOptions } from "../../api";  
+import { geoApiOptions, GEO_API_URL } from "../../api";
 
-const Search =({onSearchChange}) => {
-    const [search, setSearch ] = useState(null)
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
 
-    const loadOptions = (inputValue) => {
-        return fetch( 
-            `${GEO_API_URL}/cities?mainPopulation=1000000&namePrefix=${inputValue}`, 
-             geoApiOptions
-            )
-        .then((response) => response.json())
-        .then((response)=> {
-            return {
-                options: response.data.map((city) => {
-                    return {
-                        value: `${city.longitude} ${city.longitude}`,
-                        label: `${city.name}, ${city.countrycode}`,
-                    };
-                }),
-            };
-        })
-        .catch(err => console.error(err))
-    }
-
-    const handleOnChange = (searchData) => {
-        setSearch(searchData);
-        onSearchChange(searchData);
-    }
-
-    return (
-        <AsyncPaginate 
-          placeholder="search for city"
-          debounceTimeout={600}
-          value={search}
-          onChange={handleOnChange}
-          loadOptions={loadOptions}   
-        />
+  const loadOptions = (inputValue) => {
+    return fetch(`${GEO_API_URL}?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
     )
-}
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        return { 
+          
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      });
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
 
 export default Search;
