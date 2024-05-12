3864014b553f92ac02b2968bf6b0461cefae010a
mwangiryan12 <162281245+mwangiryan12@users.noreply.github.com>
2024-05-12 22:02:56 +0300

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,9 +1,11 @@
 export const geoApiOptions = {
-    method: 'GET',
+    method: "GET",
     headers: {
-        'X-RapidAPI-Key': '143c05dcf6msh8d023473f1de370p14ae62jsn61bb0988c5a2',
-        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    }
+      "X-RapidAPI-Key": "61f3a36370msh64ac9dbcc9c2ba8p1a4a19jsn000269b2b016",// enter your rapid api key here
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
   };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
   
-  export const GEO_API_URL = "https://wtf-geo-db.p.rapidapi.com/v1/geo";
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "3244003e0169aab7a2182966fb865021"; // enter your key from openweather API
